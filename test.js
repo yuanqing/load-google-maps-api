bf19e8da30b0aff4340907f061ee056d56f2cfed
yuanqing <desk@yuanqing.sg>
2018-03-25 12:02:50 +0800

    Edit test.js

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const test = require('tape')
 const loadGoogleMapsAPI = require('./')
 
-test('loads the API', function (t) {
+test('resolves the promise to the `googleMaps` object', function (t) {
   t.plan(1)
-  loadGoogleMapsAPI().then(t.ok, t.fail)
+  loadGoogleMapsAPI().then(function (googleMaps) {
+    t.true(typeof googleMaps.Map === 'function')
+  }, t.fail)
 })
