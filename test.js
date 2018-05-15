7eead522394fcd71b10dfbaf2eb9bbaeb3e51d2b
Yuan Qing <desk@yuanqing.sg>
2018-05-15 21:58:44 +0800

    Modify test to check for duplicate calls

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 const test = require('tape')
 const loadGoogleMapsAPI = require('./')
 
-test('resolves the promise to the `googleMaps` object', function (t) {
-  t.plan(1)
-  loadGoogleMapsAPI().then(function (googleMaps) {
-    t.true(typeof googleMaps.Map === 'function')
+test('resolves the promise to the `googleMaps` object with support for duplicate calls', function (t) {
+  t.plan(2)
+
+  const promises = [loadGoogleMapsAPI(), loadGoogleMapsAPI()]
+
+  Promise.all(promises).then(function (values) {
+    t.equal(values[0], values[1])
+    t.true(typeof values[0].Map === 'function')
   }, t.fail)
 })
