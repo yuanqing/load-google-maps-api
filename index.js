6b4543484e9a7413d2b6bd6b73c1cb0a04fb337f
Yuan Qing <desk@yuanqing.sg>
2018-05-15 21:57:27 +0800

    Cache the promise

--- index.js
+++ index.js
@@ -2,45 +2,46 @@ var CALLBACK_NAME = '__googleMapsApiOnLoadCallback'
 
 var OPTIONS_KEYS = ['channel', 'client', 'key', 'language', 'region', 'v']
 
+var promise = null
+
 module.exports = function (options) {
   options = options || {}
 
-  return new Promise(function (resolve, reject) {
-    // Check if the Google Maps API has already been loaded
-    if (window.google && window.google.maps) {
-      return resolve(window.google.maps)
-    }
+  if (!promise) {
+    promise = new Promise(function (resolve, reject) {
+      // Reject the promise after a timeout
+      var timeoutId = setTimeout(function () {
+        window[CALLBACK_NAME] = function () {} // Set the on load callback to a no-op
+        reject(new Error('Could not load the Google Maps API'))
+      }, options.timeout || 10000)
 
-    // Reject the promise after a timeout
-    var timeoutId = setTimeout(function () {
-      window[CALLBACK_NAME] = function () {} // Set the on load callback to a no-op
-      reject(new Error('Could not load the Google Maps API'))
-    }, options.timeout || 10000)
-
-    // Hook up the on load callback
-    window[CALLBACK_NAME] = function () {
-      if (timeoutId !== null) {
-        clearTimeout(timeoutId)
+      // Hook up the on load callback
+      window[CALLBACK_NAME] = function () {
+        if (timeoutId !== null) {
+          clearTimeout(timeoutId)
+        }
+        resolve(window.google.maps)
+        delete window[CALLBACK_NAME]
       }
-      resolve(window.google.maps)
-      delete window[CALLBACK_NAME]
-    }
 
-    // Prepare the `script` tag to be inserted into the page
-    var scriptElement = document.createElement('script')
-    var params = ['callback=' + CALLBACK_NAME]
-    OPTIONS_KEYS.forEach(function (key) {
-      if (options[key]) {
-        params.push(key + '=' + options[key])
+      // Prepare the `script` tag to be inserted into the page
+      var scriptElement = document.createElement('script')
+      var params = ['callback=' + CALLBACK_NAME]
+      OPTIONS_KEYS.forEach(function (key) {
+        if (options[key]) {
+          params.push(key + '=' + options[key])
+        }
+      })
+      if (options.libraries && options.libraries.length) {
+        params.push('libraries=' + options.libraries.join(','))
       }
+      scriptElement.src =
+        'https://maps.googleapis.com/maps/api/js?' + params.join('&')
+
+      // Insert the `script` tag
+      document.body.appendChild(scriptElement)
     })
-    if (options.libraries && options.libraries.length) {
-      params.push('libraries=' + options.libraries.join(','))
-    }
-    scriptElement.src =
-      'https://maps.googleapis.com/maps/api/js?' + params.join('&')
+  }
 
-    // Insert the `script` tag
-    document.body.appendChild(scriptElement)
-  })
+  return promise
 }
