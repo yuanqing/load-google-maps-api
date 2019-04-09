dfb27f020f1c2f2ddeb16880acb6d5fbc4c8b47e
yuanqing <desk@yuanqing.sg>
2019-04-09 20:38:00 +0800

    Add test for the general use case (single call)

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
 const test = require('tape')
-const loadGoogleMapsAPI = require('./')
+const loadGoogleMapsApi = require('./')
 
-test('resolves the promise to the `googleMaps` object with support for duplicate calls', function (t) {
+test('resolves the promise to the `googleMaps` object', function (t) {
+  t.plan(1)
+  loadGoogleMapsApi().then(function (googleMaps) {
+    t.true(typeof googleMaps.Map === 'function')
+  }, t.fail)
+})
+
+test('resolves the promise to the `googleMaps` object, with support for duplicate calls', function (t) {
   t.plan(2)
-
-  const promises = [loadGoogleMapsAPI(), loadGoogleMapsAPI()]
-
+  const promises = [loadGoogleMapsApi(), loadGoogleMapsApi()]
   Promise.all(promises).then(function (values) {
     t.equal(values[0], values[1])
     t.true(typeof values[0].Map === 'function')
